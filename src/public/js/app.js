e679e0ff791220a48a7a7a143e28034bd361c83e
yoonion <yoonion97@gmail.com>
2022-01-23 17:34:59 +0900

    3.1 Call Controls

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,12 +3,30 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
 
 let myStream;
 
 let muted = false;
 let cameraOff = false;
 
+// 카메라 정보 가져오기
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter(device => device.kind === "videoinput");
+    cameras.forEach(camera => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      cameraSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// media(비디오, 마이크) 불러오기
 async function getMedia() {
   try {
     myStream = await navigator.mediaDevices.getUserMedia({
@@ -16,15 +34,17 @@ async function getMedia() {
       video: true,
     });
     myFace.srcObject = myStream;
+    await getCameras();
   } catch (e) {
     console.log(e);
   }
 }
 
-getMedia();
+getMedia(); // video, audio
 
 // Mic Mute Button
 function handleMuteClick() {
+  myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
   if(!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -36,6 +56,7 @@ function handleMuteClick() {
 }
 // Camera Mute Button
 function handleCameraClick() {
+  myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
   if(cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
@@ -46,5 +67,5 @@ function handleCameraClick() {
   }
 }
 
-muteBtn.addEventListener("click", handleMuteClick);
-cameraBtn.addEventListener("click", handleCameraClick);
+muteBtn.addEventListener("click", handleMuteClick); // 마이크 on/off 리스너
+cameraBtn.addEventListener("click", handleCameraClick); // camera on/off 리스너
